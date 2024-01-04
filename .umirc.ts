17d56b030a79fc11be325585a951c0bf6e2f8da2
lht <616441509@qq.com>
2024-01-04 15:04:55 +0800

    umi配置

--- .umirc.ts
+++ .umirc.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "umi";
 
 export default defineConfig({
   routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
+    { path: "/", component: "@/pages/index" },
+    // { path: "/docs", component: "./docs" },
   ],
+  history: { type: 'hash' },
+  publicPath:'./',
+  base:'/',
   npmClient: 'yarn',
 });
